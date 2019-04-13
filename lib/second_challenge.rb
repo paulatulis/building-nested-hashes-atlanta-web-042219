9fa9bc771b9f59416ba15e526c01a1b1da4b8c91
Paula Tulis <tulis.paula@gmail.com>
2019-04-13 17:44:57 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,19 +1,4 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-      }, 
-    :capulet => {
-      :patriarch => {},
-      :matriarch => {},
-      :heroine => {},
-      :heroine_friends => []
-      #your key/value pairs here!
-      }
-  }
-
+  
   
 end

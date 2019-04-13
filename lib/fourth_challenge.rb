be73ae6fd191896379de79f6bd9bc5eed8f86d5d
Paula Tulis <tulis.paula@gmail.com>
2019-04-13 17:54:20 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -24,11 +24,3 @@ def fourth_challenge
 end
 
 
-epic_tragedy[:montague][:hero][:status] = "dead"
-  epic_tragedy[:capulet][:heroine][:status] = "dead"
-  puts epic_tragedy
-  
-
-  #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
-  epic_tragedy
-end

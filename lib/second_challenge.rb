fbcac028bdde92e60319e82008b799ca19a74f7f
Paula Tulis <tulis.paula@gmail.com>
2019-04-13 17:47:00 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,12 +1,16 @@
 def second_challenge
   epic_tragedy = {
      montague: {
-       patriarch:
-       matriarch:
-       hero:
-       
+       patriarch:{},
+       matriarch:{},
+       hero:{},
+       hero_friends:[]
      },
-     capulet: {}
+     capulet: {
+       patriarch:{},
+       matriarch:{},
+       hero:{},
+       hero_friends:[]
+     }
    }
-  
 end
